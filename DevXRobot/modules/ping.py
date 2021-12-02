460711fffd7e2c84fd7dc6216e55156636c0eb52
AKD1317 <92142624+AKD1317@users.noreply.github.com>
2021-12-02 22:41:16 +0530

    Update ping.py

--- DevXRobot/modules/ping.py
+++ DevXRobot/modules/ping.py
@@ -74,7 +74,7 @@ def ping(update: Update, context: CallbackContext):
     msg = update.effective_message
 
     start_time = time.time()
-    message = msg.reply_text("𝚁𝚄𝙺𝙾 𝙱𝙾𝚂𝚂 𝚂𝙱𝚁 𝙺𝚁𝙾 𝚃𝙷𝙾𝙳𝙰🤭...")
+    message = msg.reply_text("⚡")
     end_time = time.time()
     telegram_ping = str(round((end_time - start_time) * 1000, 3)) + " ms"
     uptime = get_readable_time((time.time() - StartTime))
